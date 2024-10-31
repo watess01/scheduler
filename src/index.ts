16a629e274d51678e5785b797c77523fe61e9d4e
watess01 <swates@mobifed.com>
2024-10-31 23:50:18 +0000

    tidy

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { Scheduler } from "./model/Scheduler";
 import { Customer } from "./model/Customer";
 import { CustomerEvent } from "./model/CustomerEvent";
 import { Event, EventType } from "./model/Event";
-// create instance of scheduler
+
+// create scheduler
 const scheduler = new Scheduler();
 
-// create customers Tim, Jon, Nick
+// create customers
 const tim = new Customer("Tim");
 const jon = new Customer("Jon");
 const nick = new Customer("Nick");
@@ -19,4 +20,5 @@ scheduler.addEvent(new CustomerEvent(nick, "03:00", EventType.PrepareSandwich));
 scheduler.addEvent(new CustomerEvent(nick, "04:00", EventType.ServeSandwich));
 scheduler.addEvent(new Event("04:30", EventType.Break));
 
+// print schedule
 scheduler.printSchedule();
